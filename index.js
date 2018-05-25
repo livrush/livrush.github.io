427aa2b39190045686e7d95b9f6cbf0a02a9f00c
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-05-25 15:04:35 -0500

    add slide logic

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 let counter = 0;
 
+const getScreenWidth = () => window.innerWidth;
+
 $(document).ready(function() {
   $('#slide-left').click(function() {
+    const screenWidth = getScreenWidth();
+    let updateAmount;
+    if (screenWidth > 768) {
+      updateAmount = 50;
+    } else {
+      updateAmount = 100;
+    }
+
     if (counter > 0) {
-      counter -= 50;
+      counter -= updateAmount;
       if (counter === 0) {
         $('.display-main').removeClass('half');
         $('.display-main-section').removeClass('full');
@@ -11,10 +21,22 @@ $(document).ready(function() {
         $('.display-main-section').addClass('half');
       }
     }
+
+    $('.projects-container').css({
+      transform: `translateX(-${counter}vw)`,
+    });
     console.log('counter is:', counter);
   })
 
   $('#slide-right').click(function() {
+    const screenWidth = getScreenWidth();
+    let updateAmount;
+    if (screenWidth > 768) {
+      updateAmount = 50;
+    } else {
+      updateAmount = 100;
+    }
+
     if (counter === 0) {
       $('.display-main').removeClass('full');
       $('.display-main-section').removeClass('half');
@@ -25,7 +47,7 @@ $(document).ready(function() {
         transform: `translateX(-${counter}vw)`,
       });
     }
-    counter += 50;
+    counter += updateAmount;
     console.log('counter is:', counter);
   })
 
