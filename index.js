b43aa8ef0af7e4b216b9a139c252d4fd50b301e2
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-05-25 15:25:08 -0500

    update sliding

--- index.js
+++ index.js
@@ -1,54 +1,37 @@
-let counter = 0;
+let horizontalSlide = 0;
+let verticalSlide = 0;
+
 
 const getScreenWidth = () => window.innerWidth;
 
 $(document).ready(function() {
   $('#slide-left').click(function() {
     const screenWidth = getScreenWidth();
-    let updateAmount;
-    if (screenWidth > 768) {
-      updateAmount = 50;
-    } else {
-      updateAmount = 100;
-    }
-
-    if (counter > 0) {
-      counter -= updateAmount;
-      if (counter === 0) {
-        $('.display-main').removeClass('half');
-        $('.display-main-section').removeClass('full');
-        $('.display-main').addClass('full');
-        $('.display-main-section').addClass('half');
-      }
-    }
-
+    horizontalSlide -= 100;
     $('.projects-container').css({
-      transform: `translateX(-${counter}vw)`,
+      transform: `translateX(-${horizontalSlide}vw)`,
     });
-    console.log('counter is:', counter);
-  })
+    console.log('horizontal placement is:', horizontalSlide);
+  });
 
   $('#slide-right').click(function() {
     const screenWidth = getScreenWidth();
-    let updateAmount;
-    if (screenWidth > 768) {
-      updateAmount = 50;
-    } else {
-      updateAmount = 100;
-    }
+    horizontalSlide += 100;
+    $('.projects-container').css({
+      transform: `translateX(-${horizontalSlide}vw)`,
+    });
+    console.log('horizontal placement is:', horizontalSlide);
+  });
 
-    if (counter === 0) {
-      $('.display-main').removeClass('full');
-      $('.display-main-section').removeClass('half');
-      $('.display-main').addClass('half');
-      $('.display-main-section').addClass('full');
-    } else if (counter > 49) {
-      $('.projects-container').css({
-        transform: `translateX(-${counter}vw)`,
-      });
-    }
-    counter += updateAmount;
-    console.log('counter is:', counter);
-  })
+  $('#slide-info').click(function() {
+    const screenWidth = getScreenWidth();
+    if (verticalSlide) verticalSlide -= 100;
+    else verticalSlide += 100;
 
-})
+    $('.projects-container').css({
+      transform: `translateX(-${horizontalSlide}vw) translateY(-${verticalSlide}vw)`,
+    });
+    console.log('vertical placement is:', verticalSlide);
+  });
+
+});
