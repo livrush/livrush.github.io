c90ad1a2813ace89a7089e6e6da2c48795280226
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-05-25 19:57:44 -0500

    fix error in vertical slide

--- index.js
+++ index.js
@@ -8,20 +8,30 @@ $(document).ready(function() {
   $('#slide-left').click(function() {
     if (horizontalSlide) {
       horizontalSlide -= 1;
+      verticalSlide = 0;
       $('.projects-container').css({
-        transform: `translateX(-${horizontalSlide * 100}vw)`,
+        // transform: `translateX(-${horizontalSlide * 100}vw)`,
+        transform: `translate(-${horizontalSlide * 100}vw, ${verticalSlide}vh)`,
       });
+      console.group('left');
       console.log('horizontal placement is:', horizontalSlide);
+      console.log('vertical placement is:', verticalSlide);
+      console.groupEnd();
     }
   });
 
   $('#slide-right').click(function() {
     const screenWidth = getScreenWidth();
     horizontalSlide += 1;
+    verticalSlide = 0;
     $('.projects-container').css({
-      transform: `translateX(-${horizontalSlide * 100}vw)`,
+      // transform: `translateX(-${horizontalSlide * 100}vw)`,
+      transform: `translate(-${horizontalSlide * 100}vw, ${verticalSlide}vh)`,
     });
+    console.group('right');
     console.log('horizontal placement is:', horizontalSlide);
+    console.log('vertical placement is:', verticalSlide);
+    console.groupEnd();
   });
 
   $('#slide-info').click(function() {
@@ -30,9 +40,12 @@ $(document).ready(function() {
     else verticalSlide += 100;
 
     $('.projects-container').css({
-      transform: `translateX(-${horizontalSlide}vw) translateY(-${verticalSlide}vh)`,
+      transform: `translate(-${horizontalSlide * 100}vw, -${verticalSlide}vh)`,
     });
+    console.group('info');
+    console.log('horizontal placement is:', horizontalSlide);
     console.log('vertical placement is:', verticalSlide);
+    console.groupEnd();
   });
 
 });
