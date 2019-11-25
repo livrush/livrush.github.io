aa79cfa3362ca09787badc63ea3a15da100482ef
Liv <w.phrush@gmail.com>
2019-11-24 19:54:41 -0600

    remove header/footer element/styles

--- index.js
+++ index.js
@@ -79,9 +79,9 @@ $(document).ready(function() {
   $(window).on('keyup', (event) => {
     console.log(event, event.key, event.which, event.keyCode);
     if (event.keyCode === 37) $('#slide-left').click();
-    else if (event.keyCode === 38) $('#slide-info').click();
+    // else if (event.keyCode === 38) $('#slide-info').click();
     else if (event.keyCode === 39) $('#slide-right').click();
-    else if (event.keyCode === 40) $('#slide-info').click();
+    // else if (event.keyCode === 40) $('#slide-info').click();
   });
 
   // $(window).on('keydown', (event) => {
